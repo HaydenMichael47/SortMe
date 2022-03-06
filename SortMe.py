b4f6ee4c5650622cd0295f76b74c6d6903a20548
Hayden Michael <haydentmichael@gmail.com>
2022-03-05 20:38:37 -0500

    Began sorting algorithm by sorting by length

--- SortMe.py
+++ SortMe.py
@@ -1,1 +1,13 @@
-print("Hello world")
+lines = []
+with open('Sort Me.txt') as f:
+    lines = f.readlines()
+
+for x in lines:
+    x.replace(" ", "")
+
+def sort(fileStrings):
+    lines.sort(key= len)
+    for x in fileStrings:
+        print(x)
+
+sort(lines)
