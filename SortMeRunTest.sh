ff296578da0c4805ffdb424e257deb754767cb4b
Hayden Michael <haydentmichael@gmail.com>
2022-04-12 11:38:41 -0400

    Updated sort Test

--- SortMeRunTest.sh
+++ SortMeRunTest.sh
@@ -1,6 +1,8 @@
 python3 SortMe.py >tmpFile.txt
 
-diff -q -w -b tmpFile.txt SortMeAnswer.txt; 
-
+if ! diff -q -w -b tmpFile.txt SortMeAnswer.txt; 
+then
+    echo "Sort Failed"
+fi
 
 rm tmpFile.txt
