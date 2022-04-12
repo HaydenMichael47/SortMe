181fc6a65747e7f62fe42b18566424e07d35fb8b
Hayden Michael <haydentmichael@gmail.com>
2022-04-12 17:32:36 -0400

    Changed test to use exit code 2

--- SortMeRunTest.sh
+++ SortMeRunTest.sh
@@ -1,8 +1,10 @@
 python3 SortMe.py >tmpFile.txt
 
-if ! diff -q -w -b tmpFile.txt SortMeAnswer.txt; 
+if ! diff -w -b tmpFile.txt SortMeAnswer.txt; 
 then
-    read -p "Sort Failed"
+    rm tmpFile.txt
+    exit 2
 fi
 
 rm tmpFile.txt
+read -p "Execution complete."
